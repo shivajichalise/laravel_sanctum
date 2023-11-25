21e66bac49364b1162823121368fc0fae6f881b9
Shivaji Chalise <respond2shivaji@gmail.com>
2023-11-25 15:59:44 +0545

    added token expiration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        // Remove expired token that has been expired for 24+ hours
+        $schedule->command('sanctum:prune-expired --hours=24')->daily();
     }
 
     /**
