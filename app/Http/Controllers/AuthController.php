a085bdbc80dd9b8c4c0d581a47574f3b29d5ce32
Shivaji Chalise <respond2shivaji@gmail.com>
2023-11-25 15:17:30 +0545

    implemented login route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -12,14 +14,25 @@ class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function authenticate()
+    public function authenticate(LoginUserRequest $request)
     {
-        return 'login';
+        $request->validated();
+
+        if(!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials do not match.', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of '. $user->name)->plainTextToken,
+        ]);
     }
 
     public function register(StoreUserRequest $request): JsonResponse
     {
-        $request->validated($request->all());
+        $request->validated();
 
         $user = User::create([
             'name' => $request->name,
